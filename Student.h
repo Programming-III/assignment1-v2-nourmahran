4cfd97ba1a5eaf0691acbffd98f087b147377d8d
nourmahran <nour.nayer2008@gmail.com>
2025-11-11 09:17:33 +0200

    Update Student.h
    Completed Student Class

--- Student.h
+++ Student.h
@@ -4,9 +4,25 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student();
+    Student(string n, int i, int y, string m);
+    ~Student();
 
+    void setYearLevel(int y);
+    void setMajor(string m);
+    int getYearLevel();
+    string getMajor();
+
+    void display() override;
+};
+
+#endif
 
 
 
