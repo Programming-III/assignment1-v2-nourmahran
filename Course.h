e958d703050930b516dc005bd8c937de989cba2d
nourmahran <nour.nayer2008@gmail.com>
2025-11-11 09:12:54 +0200

    Update Course.h
    Completed Course Class

--- Course.h
+++ Course.h
@@ -3,8 +3,38 @@
 
 #include <string>
 #include "Student.h"
+#include "Instructor.h"
 using namespace std;
-//#write your code here
+
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    Instructor instructor;
+public:
+    Course();
+    Course(string code, string name, int max, Instructor inst);
+    ~Course();
+
+    void setCourseCode(string code);
+    void setCourseName(string name);
+    void setMaxStudents(int max);
+    void setInstructor(Instructor inst);
+
+    string getCourseCode();
+    string getCourseName();
+    int getMaxStudents();
+    int getCurrentStudents();
+
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+};
+
+#endif
+
 
 
 
