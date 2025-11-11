2b89ea73975520d1edf89f5162b90051501dbbb4
nourmahran <nour.nayer2008@gmail.com>
2025-11-11 09:10:23 +0200

    Update Person.h
    Completed Person Class

--- Person.h
+++ Person.h
@@ -3,8 +3,27 @@
 
 #include <string>
 using namespace std;
+#include <iostream>
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person();
+    Person(string n, int i);
+    ~Person();
+
+    void setName(string n);
+    void setId(int i);
+    string getName();
+    int getId();
+
+    virtual void display();
+};
+
+#endif
+
 
 
 
